2008d0a252c4c0a78ec5a3ffd76fc3a72c10efab
Vyzion Development <robertseals@vyziondevelopment.com>
2020-06-10 13:06:22 +0000

    Calculator

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
 
+
+ function dis(value) 
+ { 
+     document.getElementById("result").value+=value
+ } 
+   //displays the value of operations
+
+ function solve() 
+ { 
+     let x = document.getElementById("result").value 
+     let y = eval(x) 
+     document.getElementById("result").value = y 
+ } 
+  //solves the logic
+   
+
+ function clr() 
+ { 
+     document.getElementById("result").value = "" 
+ } 
+ //will clear
+
+ function wrong() {
+
+    document.getElementById("result").value+=value
+     if (value % 2 != 0 || value % 2 != 1)
+     {
+        document.getElementById("result").value = "error"
+     }
+ }
